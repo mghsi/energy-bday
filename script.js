36c5ad3262ea0ce156db108f1eb04cb265a3146b
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-02-10 20:07:22 +0530

    sidebar

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const toggleMenu = () => {
-    document.getElementById('sidebar').classList.toggle('hidden')
-};
+function toggleMenu(state) {
+    let sidebar = document.getElementById("sidebar");
+    let openBtn = document.getElementById("open-btn");
+    let closeBtn = document.getElementById("close-btn");
+
+    if (state === 'open') {
+        sidebar.classList.remove("hidden")
+        sidebar.classList.add("flex")
+        openBtn.classList.remove("animate-rotate-right")
+        closeBtn.classList.remove("animate-rotate-right")
+        openBtn.classList.add("animate-rotate-left")
+        closeBtn.classList.add("animate-rotate-left")
+        sidebar.classList.add("animate-slide-in");
+    } else {
+        sidebar.classList.add("animate-slide-out");
+        openBtn.classList.remove("animate-rotate-left")
+        closeBtn.classList.remove("animate-rotate-left")
+        setTimeout(() => {
+            openBtn.classList.add("animate-rotate-right")
+            closeBtn.classList.add("animate-rotate-right")
+            sidebar.classList.add("hidden");
+            sidebar.classList.remove("flex");
+            sidebar.classList.remove("animate-slide-out");
+        }, 300);
+    }
+}   
